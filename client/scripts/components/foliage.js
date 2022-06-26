06757dae6333c961d939b4356be439b51ddac8ed
Ariel Wexler <ariel@markforged.com>
2022-06-26 15:35:05 -0400

    Basic map pathing and improved foliage physics

--- client/scripts/components/foliage.js
+++ client/scripts/components/foliage.js
@@ -1,29 +1,45 @@
 import { canvas } from '../ui/canvas.js';
 
-function Foliage(x, y, baseAngle) {
+function Foliage(x, y, baseAngle, levelMap, getProgress) {
   let angle = 0;
   let omega = 0;
   let acc = 0;
   let px = Math.sin(baseAngle);
   let py = Math.cos(baseAngle);
-  let l1 = Math.random() * 40 + 10;
-  let l2 = Math.random() * 40 + 10;
-  let l3 = Math.random() * 40 + 10;
-  let mode = Math.random() > 0.5;
+  let l1 = 10;
+  let l2 = 10;
+  let l3 = 10;
+  let mode = false;
+  let curl1 = false;
+  let curl2 = false;
 
   function reconfigure() {
     angle = 0;
     omega = 0;
     acc = 0;
-    l1 = Math.random() * 40 + 10;
-    l2 = Math.random() * 40 + 10;
-    l3 = Math.random() * 40 + 10;
+    l1 = Math.random() * 50 + 15;
+    l2 = Math.random() * 50 + 15;
+    l3 = Math.random() * 50 + 15;
     mode = Math.random() > 0.5;
+    curl1 = Math.random() > 0.5;
+    curl2 = Math.random() > 0.5;
     if (mode) {
-      mass = (l1 + l2 + l3) / 3 / 25;
+      mass = (l1 + l2 + l3) / 3 / 15;
     } else {
-      mass = (l1 + l2) / 2 / 25;
+      mass = (l1 + l2) / 2 / 15;
     }
+    x = levelMap(x, y);
+    const x2 = levelMap(x, y-5);
+    const x3 = levelMap(x, y+5);
+    if (x < 0) {
+      x -= 5 + Math.random() * 10;
+      baseAngle = Math.atan2(x2 - x3, 10);
+    } else {
+      x += 5 + Math.random() * 10;
+      baseAngle = Math.atan2(x3 - x2, -10);
+    }
+    px = Math.sin(baseAngle);
+    py = Math.cos(baseAngle);
   }
   reconfigure();
 
@@ -31,15 +47,15 @@ function Foliage(x, y, baseAngle) {
     const dx = state.shark.getX() - x;
     const dy = state.shark.getY() - y;
     const d2 = dx * dx + dy * dy;
-    omega += (state.shark.getVX() * px + state.shark.getVY() * py) * dT / (20 + d2 * 0.005);
+    const touchForce = 45 * (state.shark.getVX() * px + state.shark.getVY() * py) * dT / (5 + d2 * 0.005);
 
     a = Date.now() * 0.001;
     const turbulence = Math.cos(x * 0.01 + y * 0.02 + a) + Math.sin(y * 0.01 + a);
-    omega += (-angle * 20.0 - omega * 3.0 + turbulence * 1.0) / mass * dT;
+    omega += (-angle * 20.0 - omega * 3.0 + turbulence * 2.0 + touchForce) / mass * dT;
     angle += omega * dT;
 
-    if (y - 100 > state.level.getProgress()) {
-      y -= canvas.height + 100;
+    if (y - 100 > getProgress()) {
+      y -= canvas.height + 200;
       reconfigure();
     }
   }
@@ -49,7 +65,7 @@ function Foliage(x, y, baseAngle) {
     ctx.translate(x, y);
     ctx.rotate(baseAngle + angle);
 
-    ctx.lineWidth = 6.0;
+    ctx.lineWidth = 8.0;
     ctx.strokeStyle = '#111';
     if (mode) {
       ctx.beginPath();
@@ -57,15 +73,35 @@ function Foliage(x, y, baseAngle) {
       ctx.lineTo(l1, 0);
       ctx.moveTo(-5, -9);
       ctx.lineTo(l2, -15 + angle * 9);
+      if (curl2) {
+        ctx.lineTo(l2 * 1.1, -18 + angle * 9);
+        ctx.lineTo(l2 * 1.2, -24 + angle * 5);
+        ctx.lineTo(l2 * 1.1, -29 + angle * 1);
+      }
       ctx.moveTo(-5, 9);
       ctx.lineTo(l3, 15 + angle * 9);
+      if (curl2) {
+        ctx.lineTo(l3 * 1.1, 18 + angle * 9);
+        ctx.lineTo(l3 * 1.2, 24 + angle * 5);
+        ctx.lineTo(l3 * 1.1, 29 + angle * 2);
+      }
       ctx.stroke();
     } else {
       ctx.beginPath();
       ctx.moveTo(-5, 0);
       ctx.lineTo(l1, 7 + angle * 9);
+      if (curl1) {
+        ctx.lineTo(l1 * 1.1, 11 + angle * 9);
+        ctx.lineTo(l1 * 1.2, 17 + angle * 5);
+        ctx.lineTo(l1 * 1.1, 22 + angle * 2);
+      }
       ctx.moveTo(-5, -9);
       ctx.lineTo(l2, -7 + angle * 9);
+      if (curl2) {
+        ctx.lineTo(l2 * 1.1, -10 + angle * 9);
+        ctx.lineTo(l2 * 1.2, -15 + angle * 5);
+        ctx.lineTo(l2 * 1.1, -20 + angle * 1);
+      }
       ctx.stroke();
     }
 
--- client/scripts/components/level.js
+++ client/scripts/components/level.js
@@ -1,18 +1,9 @@
 import { canvas } from '../ui/canvas.js';
 import Foliage from './foliage.js';
 
-function Level() {
+function Level(engineState) {
   let progress = canvas.height * 0.2;
   const foliages = [];
-
-  for (let i = 200; i > -canvas.height; ) {
-    foliages.push(new Foliage(-400 - Math.random() * 20, i, Math.random() * 0.4 - 0.2));
-    i -= 16 + Math.random() * 50;
-  }
-  for (let j = 200; j > -canvas.height; ) {
-    foliages.push(new Foliage(400 + Math.random() * 20, j, 3.14+Math.random() * 0.4 - 0.2));
-    j -= 16 + Math.random() * 50;
-  }
 
   function update(state, dT) {
     const newProgress = progress + (state.shark.getY() + canvas.height * 0.4 - progress) * 2.0 * dT;
@@ -21,10 +12,66 @@ function Level() {
     foliages.forEach((f) => f.update(state, dT));
   }
 
+  function levelWidth(y) {
+    return 600 + Math.cos(y * 0.001) * 100 + Math.cos(y * 0.002) * 50;
+  }
+
+  function levelCenter(y) {
+    return Math.cos(y * 0.001) * 100 + Math.sin(y * 0.0005) * 60;
+  }
+
+  function levelMapLeft(y) {
+    const w = levelWidth(y);
+    const cx = levelCenter(y);
+    return -w/2 + Math.cos(y * 0.01) * 10 + Math.sin(y * 0.02) * 5 + cx;
+  }
+
+  function levelMapRight(y) {
+    const w = levelWidth(y);
+    const cx = levelCenter(y);
+    return w/2 + Math.cos(y * 0.007) * 11 + Math.sin(y * 0.022) * 4 + cx;
+  }
+
+  function levelMap(x, y) {
+    if (x < 0) {
+      return levelMapLeft(y);
+    } else {
+      return levelMapRight(y);
+    }
+  }
+
   function render(ctx) {
     ctx.fillStyle = '#111';
-    ctx.fillRect(-400, -canvas.height + progress, -canvas.width, canvas.height * 2);
-    ctx.fillRect(400, -canvas.height + progress, canvas.width, canvas.height * 2);
+
+    // Left wall
+    ctx.beginPath();
+    let first = true;
+    for (let y = parseInt((-canvas.height * 1.4 + progress) / 100) * 100; y < canvas.height * 0.3 + progress; y += 100) {
+      if (first) {
+        ctx.moveTo(levelMapLeft(y), y);
+        first = false;
+      } else {
+        ctx.lineTo(levelMapLeft(y), y);
+      }
+    }
+    ctx.lineTo(-canvas.width, canvas.height * 0.4 + progress);
+    ctx.lineTo(-canvas.width, -canvas.height * 1.4 + progress);
+    ctx.fill();
+
+    // Right wall
+    ctx.beginPath();
+    first = true;
+    for (let y = parseInt((-canvas.height * 1.4 + progress) / 100) * 100; y < canvas.height * 0.3 + progress; y += 100) {
+      if (first) {
+        ctx.moveTo(levelMapRight(y), y);
+        first = false;
+      } else {
+        ctx.lineTo(levelMapRight(y), y);
+      }
+    }
+    ctx.lineTo(canvas.width, canvas.height * 0.4 + progress);
+    ctx.lineTo(canvas.width, -canvas.height * 1.4 + progress);
+    ctx.fill();
 
     foliages.forEach((f) => f.render(ctx));
   }
@@ -33,10 +80,21 @@ function Level() {
     return progress;
   }
 
+  for (let i = 200; i > -canvas.height; ) {
+    foliages.push(new Foliage(-400 - Math.random() * 10, i, Math.random() * 0.4 - 0.2, levelMap, getProgress));
+    i -= 16 + Math.random() * 50;
+  }
+  for (let j = 200; j > -canvas.height; ) {
+    foliages.push(new Foliage(400 + Math.random() * 10, j, 3.14+Math.random() * 0.4 - 0.2, levelMap, getProgress));
+    j -= 16 + Math.random() * 50;
+  }
+
   return {
     update,
     render,
     getProgress,
+    levelMapLeft,
+    levelMapRight,
   };
 }
 
