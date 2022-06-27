b62a9676279212ce1dbf44bf7ccd70df170359f8
Ariel Wexler <ariel@markforged.com>
2022-06-27 04:48:22 -0400

    Rescale to make screen size reasonable

--- client/scripts/ui/canvas.js
+++ client/scripts/ui/canvas.js
@@ -1,5 +1,7 @@
 export const canvas = document.getElementById('ui');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = window.innerWidth * 1.33;
+canvas.height = window.innerHeight * 1.33;
+canvas.style.width = '100%';
+canvas.style.height = '100%';
 
 export const ctx = canvas.getContext('2d');
